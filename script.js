0063a8a9825c8f5377563e0453b4707473bea4c0
Zord <zord.bk@gmail.com>
2025-12-27 14:02:48 +0100

    Proper shuffle

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const SPLIT_WIDTH = 4;
 const SPLIT_HEIGHT = 3;
+const SHUFFLE_STEPS = 150;
+const SHOW_NUMBERS = true;
 
 // init dom
 const video = document.querySelector("video");
@@ -69,11 +71,13 @@ function onFrame() {
       canvasWidth,
       canvasHeight
     );
-    context.font = "3rem Rubik, system-ui, sans-serif";
-    context.textAlign = "center";
-    context.textBaseline = "middle";
-    context.fillStyle = "#7e9b94";
-    context.fillText(originalIndex, canvasWidth / 2, canvasHeight / 2);
+    if (SHOW_NUMBERS) {
+      context.font = "3rem Rubik, system-ui, sans-serif";
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillStyle = "#7e9b94";
+      context.fillText(originalIndex, canvasWidth / 2, canvasHeight / 2);
+    }
   }
   if (!video.paused && !video.ended) {
     video.requestVideoFrameCallback(onFrame);
@@ -84,6 +88,14 @@ function isAdjacent(blockA, blockB) {
   const xDiff = Math.abs(blockA.currentX - blockB.currentX);
   const yDiff = Math.abs(blockA.currentY - blockB.currentY);
   return (xDiff === 1 && yDiff === 0) || (xDiff === 0 && yDiff === 1);
+}
+
+function swapBlocks(blockA, blockB) {
+  const { currentX: tempX, currentY: tempY } = blockA;
+  blockA.currentX = blockB.currentX;
+  blockA.currentY = blockB.currentY;
+  blockB.currentX = tempX;
+  blockB.currentY = tempY;
 }
 
 function updateBlocks() {
@@ -127,32 +139,27 @@ function onCanvasClick(event) {
   if (!isAdjacent(clickedBlock, emptyBlock)) return;
   document
     .startViewTransition(() => {
-      const { currentX: tempX, currentY: tempY } = emptyBlock;
-      emptyBlock.currentX = clickedBlock.currentX;
-      emptyBlock.currentY = clickedBlock.currentY;
-      clickedBlock.currentX = tempX;
-      clickedBlock.currentY = tempY;
+      swapBlocks(emptyBlock, clickedBlock);
       updateBlocks();
     })
     .finished.then(checkWin);
 }
 
 function shuffle() {
-  for (let i = 0; i < blocks.length; i++) {
-    const one = blocks[i];
-    const two = blocks[Math.floor(Math.random() * blocks.length)];
-    const { currentX: tempX, currentY: tempY } = one;
-    one.currentX = two.currentX;
-    one.currentY = two.currentY;
-    two.currentX = tempX;
-    two.currentY = tempY;
+  let previousChoice = null;
+  const emptyBlock = blocks.find((block) => block.isEmpty);
+  for (let index = 0; index < SHUFFLE_STEPS; index += 1) {
+    const choices = blocks.filter(
+      (block) => isAdjacent(block, emptyBlock) && block !== previousChoice
+    );
+    const chosenBlock = choices[Math.floor(Math.random() * choices.length)];
+    swapBlocks(emptyBlock, chosenBlock);
+    previousChoice = chosenBlock;
   }
   document.startViewTransition(updateBlocks);
 }
 
 // TODO: remove test stuff
-video.currentTime = 15;
-
 window.stop = () => {
   video.pause();
 };
